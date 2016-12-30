7fd9a6b7c80b48d9eef19065af95f83c7734d306
Gianluca Costa <g.costa@xplico.org>
2016-12-30 17:46:19 +0000

    Tests completed. Added parameters file.

---
+++ emura_params.cmd
@@ -0,0 +1,29 @@
+# On/Off Emura 0:Off; 1:On
+on_off 1
+
+# Mode  0:auto; 1:dehumidifier; 2:air conditioning; 3:heat pump; 4:fan
+mode 4
+
+# Temperature 18 to 30 C
+temp 26
+
+# Fan 0:fan1; 1:fan2; 2:fan3; 3:fan4; 4:fan5; 5:auto; 6:night
+fan 4
+
+# Comfort 0:Off; 1:On
+comfort 0
+
+# Swing 0:Off; 1:On
+swing 0
+
+# Powerful 0:Off; 1:On
+powerful 0
+
+# Quiet 0:Off; 1:On
+quiet 0
+
+# Sensor 0:Off; 1:On
+sensor 1
+
+# Econo 0:Off; 1:On
+econo 0
